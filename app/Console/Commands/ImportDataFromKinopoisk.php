60fadadf1030dc5289cc053879577562e1a1e612
boldyrev.a <boldyrev.a@studio2i.ru>
2023-11-25 20:17:58 +0700

    Переделал команду импорта. Начал работу над импортом в базу.

--- app/Console/Commands/ImportDataFromKinopoisk.php
+++ app/Console/Commands/ImportDataFromKinopoisk.php
@@ -2,8 +2,14 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Country;
+use App\Models\Film;
+use App\Models\Genre;
+use App\Models\User;
+use App\Services\Kinopoisk\ImportUserData;
+use App\Services\KinopoiskApiUnofficial\Client;
+use Carbon\Carbon;
 use Illuminate\Console\Command;
-use Symfony\Component\DomCrawler\Crawler;
 
 class ImportDataFromKinopoisk extends Command
 {
@@ -26,79 +32,119 @@ class ImportDataFromKinopoisk extends Command
      */
     public function handle()
     {
-        $films = [
-            'votes' => $this->importVotes(),
-            'watch_lists' => $this->importWatchLists(),
-        ];
+        /** @var User $user */
+        $user = User::find(1);
 
-        file_put_contents(storage_path('import/data.json'), json_encode($films, JSON_UNESCAPED_UNICODE));
-    }
+        $filepath_votes = storage_path('import/votes_data.json');
+        $filepath_watch_lists = storage_path('import/watch_lists_data.json');
 
-    protected function importVotes()
-    {
-        $votes = [];
+        if (file_exists($filepath_votes) || file_exists($filepath_watch_lists)) {
+            $votes = json_decode(file_get_contents($filepath_votes));
+            $watch_lists = json_decode(file_get_contents($filepath_watch_lists));
+        } else {
+            $films = ImportUserData::parse();
+            $votes = $films['votes'];
+            $watch_lists = $films['watch_lists'];
 
-        foreach (glob(storage_path('import/votes/*')) as $path) {
-            if (is_dir($path)) {
-                continue;
-            }
-
-            $html = file_get_contents($path);
-            $crawler = new Crawler($html);
-
-            $crawler->filter('.profileFilmsList .item')->each(function (Crawler $crawler) use (&$votes) {
-                $id = $crawler->filter('.rateNow')->attr('mid');
-                $vote = $crawler->filter('.rateNow')->attr('vote');
-
-                if (! array_key_exists($id, $votes)) {
-                    $votes[$id] = [
-                        'vote' => $vote,
-                        'date' => $crawler->filter('.date')->text(),
-                    ];
-                }
-            });
+            file_put_contents($filepath_votes, json_encode($votes), JSON_UNESCAPED_UNICODE);
+            file_put_contents($filepath_watch_lists, json_encode($watch_lists), JSON_UNESCAPED_UNICODE);
         }
 
-        return $votes;
-    }
+        $client = new Client();
 
-    protected function importWatchLists()
-    {
-        $watch_lists = [];
+        foreach ($votes as $film_id => $vote) {
+            /** @var Film $film */
+            $film = Film::find($film_id);
+            $film_data = $client->getFilm($film_id);
 
-        foreach (glob(storage_path('import/watchlists/*')) as $path) {
-            if (is_dir($path)) {
-                continue;
+            if (! $film) {
+                /** @var Film $film */
+                $film = Film::create([
+                    'id' => $film_data->kinopoiskId,
+                    'imdb_id' => $film_data->imdbId,
+                    'name' => [
+                        'ru' => $film_data->nameRu,
+                        'en' => $film_data->nameEn,
+                        'original' => $film_data->nameOriginal,
+                    ],
+                    'rating' => [
+                        'goodReview' => [
+                            'review' => $film_data->ratingGoodReview,
+                            'count' => $film_data->ratingGoodReviewVoteCount,
+                        ],
+                        'kinopoisk' => [
+                            'review' => $film_data->ratingKinopoisk,
+                            'count' => $film_data->ratingKinopoiskVoteCount,
+                        ],
+                        'imdb' => [
+                            'review' => $film_data->ratingImdb,
+                            'count' => $film_data->ratingImdbVoteCount,
+                        ],
+                        'filmCritics' => [
+                            'review' => $film_data->ratingFilmCritics,
+                            'count' => $film_data->ratingFilmCriticsVoteCount,
+                        ],
+                        'await' => [
+                            'review' => $film_data->ratingAwait,
+                            'count' => $film_data->ratingAwaitCount,
+                        ],
+                        'rfCritics' => [
+                            'review' => $film_data->ratingRfCritics,
+                            'count' => $film_data->ratingRfCriticsVoteCount,
+                        ],
+                    ],
+                    'year' => $film_data->year,
+                    'length' => $film_data->filmLength,
+                    'slogan' => $film_data->slogan,
+                    'description' => $film_data->description,
+                    'short_description' => $film_data->shortDescription,
+                    'type' => $film_data->type,
+                    'mpaa' => $film_data->ratingMpaa,
+                    'age_limits' => $film_data->ratingAgeLimits,
+                    'start_year' => $film_data->startYear,
+                    'end_year' => $film_data->endYear,
+                    'serial' => $film_data->serial,
+                    'short' => $film_data->shortFilm,
+                    'completed' => $film_data->completed,
+                ]);
             }
 
-            $html = file_get_contents($path);
-            $crawler = new Crawler($html);
+            $user->films()->attach($film, [
+                'vote' => $vote->vote,
+                'date' => Carbon::createFromFormat('d.m.Y, H:i', $vote->date),
+            ]);
 
-            $listName = '';
+            foreach ($film_data->countries as $country_data) {
+                $country = Country::where('name', $country_data->name)->first();
 
-            $crawler->filter('#folderList')->each(function (Crawler $crawler) use (&$listName) {
-                $item = $crawler->filter('.act');
-                if ($item->count()) {
-                    preg_match('/^([\w|\W]*)\s*\([\d]+\)/mU', $crawler->filter('.act .nameAndNum')->html(), $matches);
-                    $listName = $matches[1];
+                if ($country && $country->films()->where('id', $film->id)->count() == 0) {
+                    $film->countries()->attach($country);
+                } elseif (! $country) {
+                    $film->countries()->create([
+                        'name' => $country_data->name,
+                    ]);
                 }
-            });
+            }
 
-            $crawler->filter('#itemList li')->each(function (Crawler $crawler) use (&$watch_lists, $listName) {
-                $id = $crawler->attr('data-id');
+            foreach ($film_data->genres as $genre_data) {
+                $genre = Genre::where('name', $genre_data->name)->first();
 
-                if (array_key_exists($id, $watch_lists)) {
-                    $watch_lists[$id]['lists'][] = $listName;
-                } else {
-                    $watch_lists[$id] = [
-                        'date' => $crawler->filter('.remove')->nextAll()->first()->text(),
-                        'sing' => $crawler->filter('.sign textarea')->text(),
-                        'lists' => [$listName],
-                    ];
+                if ($genre && $genre->films()->where('id', $film->id)->count() == 0) {
+                    $film->genres()->attach($genre);
+                } elseif (! $genre) {
+                    $film->genres()->create([
+                        'name' => $genre_data->name,
+                    ]);
                 }
-            });
+            }
+
+            // TODO доделать создание сериалов
+            // TODO доделать создание людей
+            // TODO доделать создание наград
+            // TODO доделать создание дистрибуций
+            // TODO доделать создание сиквелов и приквелов
         }
 
-        return $watch_lists;
+        dd(112);
     }
 }
--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Services\KinopoiskApiUnofficial\Client;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,11 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule
+            ->command('import-data-from-kinopoisk')
+            ->skip((new Client())->hasLimitReached())
+            ->everyMinute()
+            ->withoutOverlapping();
     }
 
     /**
