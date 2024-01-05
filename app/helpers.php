daf9d7f367c46665e80ac593eb76cf96fe612b6a
boldyrev.a <boldyrev.a@studio2i.ru>
2024-01-05 12:56:56 +0700

    Поправил хелпер цвета по оценке

--- app/helpers.php
+++ app/helpers.php
@@ -3,18 +3,6 @@
 if (!function_exists('get_vote_class')) {
     function get_vote_class(int $vote)
     {
-        return match ($vote) {
-            0 => 'vote-0',
-            1 => 'vote-1',
-            2 => 'vote-2',
-            3 => 'vote-3',
-            4 => 'vote-4',
-            5 => 'vote-5',
-            6 => 'vote-6',
-            7 => 'vote-7',
-            8 => 'vote-8',
-            9 => 'vote-9',
-            10 => 'vote-10',
-        };
+        return 'vote-' . $vote;
     }
 }
