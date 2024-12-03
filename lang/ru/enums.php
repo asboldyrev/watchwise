03666f446cf1175ab2c7a24489c53c4a611793c8
boldyrev.a <boldyrev.a@studio2i.ru>
2024-12-04 00:35:31 +0700

    Добавил файлы локализации

---
+++ lang/ru/enums.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'SEQUEL' => 'Сиквел',
+    'PREQUEL' => 'Приквел',
+    'REMAKE' => 'Ремейк',
+    'UNKNOWN' => 'Неизвестно',
+
+    'LOCAL' => 'Локальная',
+    'COUNTRY_SPECIFIC' => 'Особый для страны',
+    'PREMIERE' => 'Премьера',
+    'ALL' => 'Все',
+    'WORLD_PREMIER' => 'Мировая премьера',
+
+    'CINEMA' => 'Кинотеатр',
+    'DVD' => 'DVD',
+    'DIGITAL' => 'Цифровой релиз',
+    'BLURAY' => 'BlueRay',
+];
