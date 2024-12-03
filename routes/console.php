910efc28c62d0b2408fb6ca2d0c4837d6ca10052
boldyrev.a <boldyrev.a@studio2i.ru>
2024-12-04 01:16:40 +0700

    Добавил в расписание команду обработки очереди

--- routes/console.php
+++ routes/console.php
@@ -1,8 +1,27 @@
 <?php
 
+use App\Events\FilmImported;
+use App\Models\Film;
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+// Artisan::command('inspire', function () {
+//     $this->comment(Inspiring::quote());
+// })->purpose('Display an inspiring quote');
+
+Schedule
+    ::command(
+        'queue:work',
+        [
+            // '--stop-when-empty',
+            '--tries' => 3,
+            '--max-time' => 300,
+        ]
+    )
+    ->skip(fn() => DB::table('jobs')->count() == 0)
+    ->everyMinute()
+    ->withoutOverlapping();
+
+// Schedule::command('reverb:start')->everyMinute()->withoutOverlapping();
