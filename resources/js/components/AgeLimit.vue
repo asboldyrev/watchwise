3f841b88fb0bf68fa967259e1a89f1cae664787f
asboldyrev <angelcorpc@ya.ru>
2024-12-07 17:52:54 +0700

    Поправил вёрстку страницы фильма

--- resources/js/components/AgeLimit.vue
+++ resources/js/components/AgeLimit.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="position-absolute ms-1 mt-1" v-if="film.mpaa || film.age">
+    <div class="position-absolute ms-2 mt-2" v-if="film.mpaa || film.age">
         <div v-if="film.mpaa" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black me-2" style="min-width: 40px;">
             <small>
                 <strong>{{ film.mpaa }}</strong>
--- resources/js/pages/Film.vue
+++ resources/js/pages/Film.vue
@@ -3,19 +3,22 @@
         <div class="col-sm-3">
             <div class="mb-4">
                 <AgeLimit :film="film" />
-                <img class="poster img-fluid" :src="film.images?.poster?.[0]?.media?.original_url">
+                <img class="poster rounded img-fluid" :src="film.images?.poster?.[0]?.media?.original_url">
             </div>
         </div>
 
         <div class="col-sm-9">
             <h1>
                 {{ name }}
-                <span v-if="film.name?.ru && (film.name.ru || film.name.original)" class="h4 text-secondary">
-                    ({{ film.name.en ? film.name.en : film.name.original }})
-                </span>
+                <span v-if="film.serial && film.start_year == film.end_year">({{ film.start_year }})</span>
+                <span v-else-if="film.serial">({{ film.start_year }} - {{ film.end_year ? film.end_year : '...' }})</span>
+                <span v-else>({{ film.year }})</span>
             </h1>
+            <h3 v-if="film.name?.ru && (film.name.ru || film.name.original)" class="h4 text-secondary">
+                {{ film.name.en ? film.name.en : film.name.original }}
+            </h3>
 
-            <ul class="nav nav-tabs" role="tablist">
+            <ul class="nav nav-tabs mt-4" role="tablist">
                 <li class="nav-item" role="presentation">
                     <button class="nav-link" :class="{ 'active': currentTab == 'main' }" @click="currentTab = 'main'" type="button" role="tab">Основное</button>
                 </li>
@@ -58,13 +61,26 @@
                                 <em>{{ film.slogan }}</em>
                             </p>
                             <p>{{ film.description }}</p>
-                            <div class="mt-3">
+                            <!-- <div class="mt-3">
                                 <a class="btn btn-outline-secondary btn-sm" v-if="film.serial" :href="`https://www.kinopoisk.ru/series/${film.id}`" target="_blank">Кинопоиск</a>
                                 <a class="btn btn-outline-secondary btn-sm" v-else :href="`https://www.kinopoisk.ru/film/${film.id}`" target="_blank">Кинопоиск</a>
                                 <a v-if="film.imdb_id" class="btn btn-outline-secondary btn-sm ms-2" :href="`https://www.imdb.com/title/${film.imdb_id}`" target="_blank">IMDB</a>
-                            </div>
+                            </div> -->
                             <p class="mt-3"><strong>Год</strong>: {{ film.year }}</p>
                             <p><strong>Длина</strong>: {{ film.length }} мин.</p>
+
+                            <div class="mt-4" v-if="film.theaters?.length">
+                                <h5>Посмотреть</h5>
+                                <div class="row g-1">
+                                    <div v-for="theater in film.theaters" class="col-auto">
+                                        <a style="display: inline-block; overflow: hidden;" class="btn btn-outline-secondary btn-sm ps-0 py-0" target="_blank" :href="theater.pivot.url">
+                                            <img v-if="theater.images?.logo?.[0]?.urls?.origin" class="img-fluid me-1" width="30" :src="theater.images.logo[0].urls.origin" alt="">
+                                            <span>{{ theater.name }}</span>
+                                        </a>
+                                    </div>
+
+                                </div>
+                            </div>
                         </div>
 
 
@@ -76,23 +92,11 @@
                                         <li v-if="data.review" class="list-group-item d-flex justify-content-between align-items-start">
                                             <div class="me-auto ms-2">
                                                 {{ name }}
-                                                <span class="small" v-if="data.count">({{ data.count }})</span>
+                                                <span class="small" v-if="data.count">({{ votes(data.count) }})</span>
                                             </div>
                                             <span class="badge bg-primary rounded-pill">{{ data.review }}</span>
                                         </li>
                                     </template>
-                                </ul>
-                            </div>
-
-                            <div class="card mt-4" v-if="film.theaters?.length">
-                                <div class="card-header">Онлайн кинотеатры</div>
-                                <ul class="list-group list-group-flush">
-                                    <li class="list-group-item" v-for="theater in film.theaters">
-                                        <a class="text-decoration-none mx-2" target="_blank" :href="theater.pivot.url">
-                                            <img v-if="theater.images?.logo?.[0]?.urls?.origin" class="img-fluid me-2" width="30" :src="theater.images.logo[0].urls.origin" alt="">
-                                            <span>{{ theater.name }}</span>
-                                        </a>
-                                    </li>
                                 </ul>
                             </div>
                         </div>
@@ -165,6 +169,18 @@
             film.value = response.data
             loaded.value = true
         })
+    }
+
+    function votes(votes) {
+        if (votes > 1000000) {
+            return Math.round(votes / 1000000) + ' млн.'
+        }
+
+        if (votes > 1000) {
+            return Math.round(votes / 1000) + ' тыс.'
+        }
+
+        return votes
     }
 
     const name = computed(() => {
