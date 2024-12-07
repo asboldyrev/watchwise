f907472a63b1913fde93f967f9af244049749205
asboldyrev <angelcorpc@ya.ru>
2024-12-07 21:55:40 +0700

    Поправил компонент AgeLimit, сделал возможно переключать размер

--- resources/js/components/AgeLimit.vue
+++ resources/js/components/AgeLimit.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="position-absolute ms-2 mt-2" v-if="film.mpaa || film.age">
-        <div v-if="film.mpaa" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black me-2" style="min-width: 40px;">
+        <div :class="{ 'small': small }" v-if="film.mpaa" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black me-2" style="min-width: 40px;">
             <small>
                 <strong>{{ film.mpaa }}</strong>
             </small>
         </div>
-        <div v-if="film.age" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black" style="min-width: 40px;">
+        <div :class="{ 'small': small }" v-if="film.age" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black" style="min-width: 40px;">
             <small>
                 <strong>{{ film.age }}</strong>
             </small>
@@ -18,7 +18,11 @@
         film: {
             type: Object,
             required: true,
-        }
+        },
+        small: {
+            type: Boolean,
+            default: false,
+        },
     })
 </script>
 
