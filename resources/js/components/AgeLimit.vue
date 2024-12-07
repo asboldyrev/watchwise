65fccd621768d0112d4ebd08b0adf1ce90d0e11a
asboldyrev <angelcorpc@ya.ru>
2024-12-07 15:48:41 +0700

    Поправил разметку меток рейтингов

--- resources/js/components/AgeLimit.vue
+++ resources/js/components/AgeLimit.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="position-absolute ms-1 mt-1" v-if="film.mpaa || film.age">
-        <div v-if="film.mpaa" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black" style="min-width: 40px;">
+        <div v-if="film.mpaa" class="d-inline-block bg-light text-uppercase rounded border border-2 border-black px-2 py-1 text-center text-black me-2" style="min-width: 40px;">
             <small>
                 <strong>{{ film.mpaa }}</strong>
             </small>
