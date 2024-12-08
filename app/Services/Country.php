bafe66d2f276cdf0e84312cc10fd9baa2849f8c0
asboldyrev <angelcorpc@ya.ru>
2024-12-08 20:31:46 +0700

    Добавил в класс app/Services/Country селектор для «неправильных» названий стран

--- app/Services/Country.php
+++ app/Services/Country.php
@@ -9,9 +9,13 @@ class Country
         $file = file_get_contents(database_path('countries.json'));
         $countries = collect(json_decode($file));
 
-        if ($countryName == 'США') {
-            $countryName = 'Соединенные штаты';
-        }
+        $countryName = match ($countryName) {
+            'США' => 'Соединенные штаты',
+            'Чехия' => 'Чешская Республика',
+            'Иордания' => 'Джордан',
+            'ОАЭ' => 'Объединенные Арабские Эмираты',
+            default => $countryName,
+        };
 
         return $countries
             ->filter(fn($country) => mb_strtolower($country->translations->ru) == mb_strtolower($countryName))
