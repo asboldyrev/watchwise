ade32e61ea44fe8c743c9fa90f6471bcf0f9522b
asboldyrev <angelcorpc@ya.ru>
2024-12-10 13:58:30 +0700

    Добавил в команду синхронизации фильмов возможность указывать id фильмов

--- app/Console/Commands/SyncFilms.php
+++ app/Console/Commands/SyncFilms.php
@@ -5,6 +5,7 @@ namespace App\Console\Commands;
 use App\Jobs\SyncFilmDataJob;
 use App\Models\Film;
 use Illuminate\Console\Command;
+use Illuminate\Support\Str;
 
 class SyncFilms extends Command
 {
@@ -13,7 +14,7 @@ class SyncFilms extends Command
      *
      * @var string
      */
-    protected $signature = 'app:sync-films';
+    protected $signature = 'app:sync-films {--ids=*}';
 
     /**
      * The console command description.
@@ -27,10 +28,30 @@ class SyncFilms extends Command
      */
     public function handle()
     {
-        $films = Film::all();
+        $ids = $this->getIds();
+        if (count($ids) > 0) {
+            $films = Film::whereIn('id', $ids)->get();
+        } else {
+            $films = Film::all();
+        }
 
         foreach ($films as $film) {
             SyncFilmDataJob::dispatch($film->id);
         }
+    }
+
+    protected function getIds(): array
+    {
+        $mediaIds = $this->option('ids');
+
+        if (! is_array($mediaIds)) {
+            $mediaIds = explode(',', (string) $mediaIds);
+        }
+
+        if (count($mediaIds) === 1 && Str::contains((string) $mediaIds[0], ',')) {
+            $mediaIds = explode(',', (string) $mediaIds[0]);
+        }
+
+        return $mediaIds;
     }
 }
