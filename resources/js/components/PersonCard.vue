45da0ee7b47b702c238f7cfa7af7c929e91dda09
asboldyrev <angelcorpc@ya.ru>
2024-12-16 20:58:57 +0700

    Изменил карточку персоны

--- resources/js/components/PersonCard.vue
+++ resources/js/components/PersonCard.vue
@@ -1,13 +1,22 @@
 <template>
     <router-link :to="{ name: 'persons.show', params: { person: person.id } }" class="card text-decoration-none">
-        <img loading="lazy" v-if="person.images?.poster?.[0]?.urls?.origin" class="card-img-top img-fluid" :src="person.images?.poster?.[0]?.urls?.origin" :alt="person.name?.ru ? person.name.ru : person.name.en">
-        <div class="card-body">
-            <h5 class="card-title">{{ person.name?.ru ? person.name.ru : person.name.en }}</h5>
+        <div class="row g-0">
+            <div class="col-md-4">
+                <img loading="lazy" v-if="person.images?.poster?.[0]?.urls?.origin" class="img-fluid" :src="person.images?.poster?.[0]?.urls?.origin" :alt="person.name?.ru ? person.name.ru : person.name.en">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">{{ person.name?.ru ? person.name.ru : person.name.en }}</h5>
+                    <p v-if="person?.pivot?.description" class="card-text description">{{ person.pivot.description }}</p>
+                    <!-- <p class="card-text"><small class="text-body-secondary">{{ genres }}</small></p> -->
+                    <!-- <span class="badge text-bg-light" v-if="related">{{ relatedFilm }}</span> -->
+
+                    <!-- <div>
+                        <CountryFlag v-for="country in film.countries" :country="country" />
+                    </div> -->
+                </div>
+            </div>
         </div>
-        <ul class="list-group list-group-flush">
-            <!-- <li class="list-group-item">{{ person.pivot.profession_text }}</li> -->
-            <li v-if="person?.pivot?.description" class="list-group-item">{{ person.pivot.description }}</li>
-        </ul>
     </router-link>
 </template>
 
@@ -20,4 +29,9 @@
     })
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+    .card {
+        text-decoration: none;
+        overflow: hidden;
+    }
+</style>
