45d8bce610baeafe847adbad249ee2bcd52610a6
asboldyrev <angelcorpc@ya.ru>
2024-12-24 22:24:19 +0700

    Доделал форму поиска. Добавил событие на enter в поле

--- resources/js/components/SearchForm.vue
+++ resources/js/components/SearchForm.vue
@@ -1,17 +1,27 @@
 <template>
     <div class="d-flex" role="search">
-        <input class="form-control me-2" min="2" type="search" v-model="query" placeholder="Титаник" aria-label="Search">
-        <router-link class="btn btn-outline-success" :to="{ name: 'search', query: { query } }">Найти</router-link>
+        <div class="input-group">
+            <input class="form-control" @keyup.enter="search" min="2" type="search" v-model="query" placeholder="Титаник" aria-label="Search">
+            <router-link class="btn btn-outline-secondary" :to="{ name: 'search', query: { query } }">
+                <i class="ri-search-line"></i>
+            </router-link>
+        </div>
+
     </div>
 
 </template>
 
 <script setup>
     import { ref, watch } from 'vue'
-    import { useRoute } from 'vue-router'
+    import { useRoute, useRouter } from 'vue-router'
 
     const route = useRoute()
+    const router = useRouter()
     const query = ref(route.query?.query)
+
+    function search() {
+        router.push({ name: 'search', query: { query: query.value } })
+    }
 
     watch(route, (newQuery) => {
         query.value = newQuery.query?.query
